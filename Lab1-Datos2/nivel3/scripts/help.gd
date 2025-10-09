e0838bf482ef86983250ba5dd9b38210f3bb577e
Sahory-24 <sahoryb@uninorte.edu.co>
2025-10-09 12:39:14 -0500

    Buttons level 3

--- Lab1-Datos2/nivel3/scripts/help.gd
+++ Lab1-Datos2/nivel3/scripts/help.gd
@@ -11,9 +11,13 @@ func _ready():
 
 
 func _on_next_pressed() -> void:
+	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
+	AudioManager.SFXPlayer.play()
 	ayuda1.visible = false
 	ayuda2.visible = true
 
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://nivel3/scenes/MemoryGame.tscn")
+	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
+	AudioManager.SFXPlayer.play()
+	SceneTransitions.change_scene_to_file("res://nivel3/scenes/MemoryGame.tscn")
--- Lab1-Datos2/nivel3/scripts/inicio_level_3.gd
+++ Lab1-Datos2/nivel3/scripts/inicio_level_3.gd
@@ -2,4 +2,7 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://nivel3/scenes/MemoryGame.tscn")
+	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
+	AudioManager.SFXPlayer.play()
+	SceneTransitions.change_scene_to_file("res://nivel3/scenes/MemoryGame.tscn")
+	
