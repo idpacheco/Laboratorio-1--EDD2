a0a6f957078438ad9eafc0e4defa052eee3eb05a
Danna <galavisd@uninorte.edu.co>
2025-10-08 22:26:10 -0500

    Configuración y sonidos

--- Lab1-Datos2/mainMenu/Scripts/main.gd
+++ Lab1-Datos2/mainMenu/Scripts/main.gd
@@ -2,5 +2,14 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
+	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
+	AudioManager.SFXPlayer.play()
 	SceneTransitions.change_scene_to_file("res://Level 2/scenes/inicio.tscn")
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	AudioManager.SFXPlayer.stream = preload("res://mainMenu/Assets/Audio/tf2-button-click-hover.mp3")
+	AudioManager.SFXPlayer.play()
+	SceneTransitions.change_scene_to_file("res://mainMenu/Scenes/Config.tscn")
 	pass # Replace with function body.
